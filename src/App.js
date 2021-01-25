79fe2d23b3c9206a7bbba807d19d0e15e8196db8
yourgurunet <info@yourgurunet.com>
2021-01-25 06:43:00 +0200

    Reducer setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import Counter from './Counter';
+import { createStore } from 'redux';
 
-function App() {
+// Reducer setup
+const reducer = () => {
+  console.log('Reducer works');
+};
+
+// Store setup
+const store = createStore(reducer);
+
+const App = () => {
   return <Counter />;
-}
+};
 
 export default App;
