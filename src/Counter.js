bea859dff018ec9c01220dc9b9caf20d2323f528
yourgurunet <info@yourgurunet.com>
2021-01-25 06:17:32 +0200

    Simple function

--- src/Counter.js
+++ src/Counter.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 const Counter = () => {
   const [count, setCount] = React.useState(0);
-  return <h1>Counter</h1>;
+  return (
+    <div className='container'>
+      <h1>Counter</h1>
+      <p className='counter'>{count}</p>
+      <div className='buttons'>
+        <button className='btn' onClick={() => setCount(count - 1)}>
+          decrease
+        </button>
+        <button className='btn' onClick={() => setCount(0)}>
+          restart
+        </button>
+        <button className='btn' onClick={() => setCount(count + 1)}>
+          increase
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
